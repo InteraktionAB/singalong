bdbe016ed11c58899b375f215d615d146f77d3d4
Sreerag M <sreeragibtl@gmail.com>
2022-08-25 13:54:04 +0530

    Add docstr-coverage workflow (#19)
    * Add docstr-coverage workflow
    
    * Fix indentation error in docstr-coverage workflow
    
    * Add package singalong
    
    * Add docstring

--- test/test_bypass.py
+++ test/test_bypass.py
@@ -1,2 +1,20 @@
+"""Bypass test for GitHub Actions workflow
+
+Many workflows require tests to exist. This dummy
+test exist to satisfy them.
+"""
+
+
 def test_bypass():
+
+    """Always pass
+
+    This test will always pass. Exist to satisfy
+    GitHub Action workflow.
+
+    Args:
+        None
+
+    Returns:
+        None"""
     assert True
