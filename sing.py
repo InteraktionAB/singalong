aa9e02e31567ac109e9681a02849665561bd1e44
Sreerag M <sreeragibtl@gmail.com>
2022-08-29 15:27:52 +0530

    Accept arguments

--- sing.py
+++ sing.py
@@ -1,10 +1,11 @@
-from typing import Any, List
+from typing import Any, List, Tuple
 
 from gradio import Audio, Dropdown
+from numpy import ndarray
 
 
-def inference() -> None:
-    pass
+def inference(sample: Tuple[int, ndarray], song: str) -> Tuple[int, ndarray]:
+    return sample
 
 
 choices: List[str] = ["Fly Me to the Moon"]
