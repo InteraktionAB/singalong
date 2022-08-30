c592242de354ea052121084f76ff7ef331239850
Sreerag M <sreeragibtl@gmail.com>
2022-08-30 12:03:04 +0530

    Add docstring

--- singalong/inference.py
+++ singalong/inference.py
@@ -1,3 +1,15 @@
+"""Define dependencies for inference
+
+This module define the dependables for the
+gradio interface.
+
+    Typical example:
+        from singalong.inference import inference, inputs, outputs
+        from gradio import Interface
+
+        interface = Interface(fn=inference, inputs=inputs, outputs=outputs)
+"""
+
 from typing import Any, List, Tuple
 
 from gradio import Audio, Dropdown
@@ -5,6 +17,19 @@ from numpy import ndarray
 
 
 def inference(sample: Tuple[int, ndarray], song: str) -> Tuple[int, ndarray]:
+    """The inference function
+
+    This function runs when data audio is connected.
+
+    Args:
+        sample: The audio sample of the format (sample_rate, audio_array).
+        song: Name of the song. Eg: Fly Me to the Moon.
+
+    Returns:
+        sample: The input audio as it is.
+
+    Raises:
+    """
     return sample
 
 
